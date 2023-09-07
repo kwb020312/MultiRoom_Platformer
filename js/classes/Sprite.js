e671e16de43710abc97b9bd98b85ed7d18ef5016
김우빈 <kwb020312@naver.com>
2023-09-07 22:45:22 +0900

    Feat: Background Sprite

---
+++ js/classes/Sprite.js
@@ -0,0 +1,14 @@
+class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position;
+    this.image = new Image();
+    this.image.onload = () => {
+      this.loaded = true;
+    };
+    this.image.src = imageSrc;
+    this.loaded = false;
+  }
+  draw() {
+    if (this.loaded) c.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
