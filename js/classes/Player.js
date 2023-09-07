566796f3e296b1edf28ce1ca68033c81513a9f8a
김우빈 <kwb020312@naver.com>
2023-09-07 21:26:47 +0900

    Feat: Player Movement

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -24,12 +24,12 @@ class Player {
   }
 
   update() {
-    this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+    this.sides.bottom = this.position.y + this.height;
 
     if (this.sides.bottom + this.velocity.y < canvas.height) {
       this.velocity.y += this.gravity;
-      this.sides.bottom = this.position.y + this.height;
     } else {
       this.velocity.y = 0;
     }
