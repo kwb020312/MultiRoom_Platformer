32351eabcb4ee10f70cdd6b74681b4a734c0098c
김우빈 <kwb020312@naver.com>
2023-09-10 21:23:26 +0900

    Feat: Collisions Detection Event

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,8 +1,8 @@
 class Player {
-  constructor() {
+  constructor({ collisionBlocks = [] }) {
     this.position = {
-      x: 100,
-      y: 100,
+      x: 200,
+      y: 200,
     };
 
     this.velocity = {
@@ -10,12 +10,14 @@ class Player {
       y: 0,
     };
 
-    this.width = 100;
-    this.height = 100;
+    this.width = 20;
+    this.height = 20;
     this.sides = {
       bottom: this.position.y + this.height,
     };
     this.gravity = 1;
+
+    this.collisionBlocks = collisionBlocks;
   }
 
   draw() {
@@ -25,13 +27,71 @@ class Player {
 
   update() {
     this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    this.sides.bottom = this.position.y + this.height;
 
-    if (this.sides.bottom + this.velocity.y < canvas.height) {
-      this.velocity.y += this.gravity;
-    } else {
-      this.velocity.y = 0;
+    // 수평 접촉 확인
+    this.checkForHorizontalCollisions();
+
+    // 중력 적용
+    this.applyGravity();
+
+    // 수직 접촉 확인
+    this.checkForVerticalCollisions();
+  }
+
+  checkForHorizontalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+
+      // 접촉 조건
+      if (
+        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        this.position.x + this.width >= collisionBlock.position.x &&
+        this.position.y + this.height >= collisionBlock.position.y &&
+        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      ) {
+        if (this.velocity.x < -0) {
+          this.position.x =
+            collisionBlock.position.x + collisionBlock.width + 0.01;
+          break;
+        }
+
+        if (this.velocity.x > 0) {
+          this.position.x = collisionBlock.position.x - this.width - 0.01;
+          break;
+        }
+      }
+    }
+  }
+
+  applyGravity() {
+    this.velocity.y += this.gravity;
+    this.position.y += this.velocity.y;
+  }
+
+  checkForVerticalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+
+      // 접촉 조건
+      if (
+        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        this.position.x + this.width >= collisionBlock.position.x &&
+        this.position.y + this.height >= collisionBlock.position.y &&
+        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      ) {
+        if (this.velocity.y < 0) {
+          this.velocity.y = 0;
+          this.position.y =
+            collisionBlock.position.y + collisionBlock.height + 0.01;
+          break;
+        }
+
+        if (this.velocity.y > 0) {
+          this.velocity.y = 0;
+          this.position.y = collisionBlock.position.y - this.height - 0.01;
+          break;
+        }
+      }
     }
   }
 }
