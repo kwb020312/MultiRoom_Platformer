61c697d356be22da1dcbf1331171586cc50215b5
김우빈 <kwb020312@naver.com>
2023-09-12 22:42:54 +0900

    Feat: Change Stage

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
-const parsedCollisions = collisionsLevel1.parse2D();
-const collisionBlocks = parsedCollisions.createObjectsFrom2D();
-
-const backgroundLevel1 = new Sprite({
-  position: {
-    x: 0,
-    y: 0,
-  },
-  imageSrc: "./img/backgroundLevel1.png",
-});
-
+let parsedCollisions;
+let collisionBlocks;
+let background;
+let doors;
 const player = new Player({
-  collisionBlocks,
   imageSrc: "./img/king/idle.png",
   frameRate: 11,
   animations: {
@@ -43,31 +35,129 @@ const player = new Player({
       frameBuffer: 4,
       loop: true,
       imageSrc: "./img/king/runLeft.png",
-      image: new Image(),
     },
     enterDoor: {
       frameRate: 8,
       frameBuffer: 4,
       loop: false,
       imageSrc: "./img/king/enterDoor.png",
-      image: new Image(),
+      onComplete: () => {
+        gsap.to(overlay, {
+          opacity: 1,
+          onComplete: () => {
+            level++;
+            if (level === 4) level = 1;
+            levels[level].init();
+            player.switchSprite("idleRight");
+            player.preventInput = false;
+            gsap.to(overlay, {
+              opacity: 0,
+            });
+          },
+        });
+      },
     },
   },
 });
 
-const doors = [
-  new Sprite({
-    position: {
-      x: 767,
-      y: 270,
+let level = 1;
+let levels = {
+  1: {
+    init: () => {
+      parsedCollisions = collisionsLevel1.parse2D();
+      collisionBlocks = parsedCollisions.createObjectsFrom2D();
+      player.collisionBlocks = collisionBlocks;
+      if (player.currentAnimation) player.currentAnimation.isActive = false;
+
+      doors = [
+        new Sprite({
+          position: {
+            x: 767,
+            y: 270,
+          },
+          imageSrc: "./img/doorOpen.png",
+          frameRate: 5,
+          frameBuffer: 10,
+          loop: false,
+          autoplay: false,
+        }),
+      ];
+
+      background = new Sprite({
+        position: {
+          x: 0,
+          y: 0,
+        },
+        imageSrc: "./img/backgroundLevel1.png",
+      });
     },
-    imageSrc: "./img/doorOpen.png",
-    frameRate: 5,
-    frameBuffer: 10,
-    loop: false,
-    autoplay: false,
-  }),
-];
+  },
+  2: {
+    init: () => {
+      parsedCollisions = collisionsLevel2.parse2D();
+      collisionBlocks = parsedCollisions.createObjectsFrom2D();
+      player.collisionBlocks = collisionBlocks;
+      player.position.x = 96;
+      player.position.y = 140;
+
+      if (player.currentAnimation) player.currentAnimation.isActive = false;
+
+      doors = [
+        new Sprite({
+          position: {
+            x: 772,
+            y: 336,
+          },
+          imageSrc: "./img/doorOpen.png",
+          frameRate: 5,
+          frameBuffer: 10,
+          loop: false,
+          autoplay: false,
+        }),
+      ];
+
+      background = new Sprite({
+        position: {
+          x: 0,
+          y: 0,
+        },
+        imageSrc: "./img/backgroundLevel2.png",
+      });
+    },
+  },
+  3: {
+    init: () => {
+      parsedCollisions = collisionsLevel3.parse2D();
+      collisionBlocks = parsedCollisions.createObjectsFrom2D();
+      player.collisionBlocks = collisionBlocks;
+      player.position.x = 750;
+      player.position.y = 230;
+      if (player.currentAnimation) player.currentAnimation.isActive = false;
+
+      doors = [
+        new Sprite({
+          position: {
+            x: 176,
+            y: 336,
+          },
+          imageSrc: "./img/doorOpen.png",
+          frameRate: 5,
+          frameBuffer: 10,
+          loop: false,
+          autoplay: false,
+        }),
+      ];
+
+      background = new Sprite({
+        position: {
+          x: 0,
+          y: 0,
+        },
+        imageSrc: "./img/backgroundLevel3.png",
+      });
+    },
+  },
+};
 
 const keys = {
   w: { pressed: false },
@@ -75,13 +165,17 @@ const keys = {
   d: { pressed: false },
 };
 
+const overlay = {
+  opacity: 0,
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
 
-  backgroundLevel1.draw();
-  collisionBlocks.forEach((collisionBlobk) => {
-    collisionBlobk.draw();
-  });
+  background.draw();
+  // collisionBlocks.forEach((collisionBlobk) => {
+  //   collisionBlobk.draw();
+  // });
 
   doors.forEach((door) => {
     door.draw();
@@ -90,6 +184,13 @@ function animate() {
   player.handleInput(keys);
   player.draw();
   player.update();
+
+  c.save();
+  c.globalAlpha = overlay.opacity;
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  c.restore();
 }
 
+levels[level].init();
 animate();
